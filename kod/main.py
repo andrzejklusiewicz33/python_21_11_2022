f4ea349312cf64b3b57630248da8bc0799522e98
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2022-11-21 10:04:10 +0100

    zadanie 2

--- kod/main.py
+++ kod/main.py
@@ -20,9 +20,9 @@
 # y=66
 # print("x={} y={}".format(x,99))
 # print(f'x={x} y={y}')
-#jakiś komentarz
-#zwierze=input('podaj nazwę zwierza:\n')
-#print(f'wybrane zwierze to {zwierze}')
+# jakiś komentarz
+# zwierze=input('podaj nazwę zwierza:\n')
+# print(f'wybrane zwierze to {zwierze}')
 
 # x=input('podaj x:\n')
 # #print(x/3)
@@ -37,3 +37,18 @@
 
 #1. Napisz program który przyjmie od uzyszkodnika jego imie oraz nazwisko, a nastepnie
 #wyswietli komunikat typu "Witaj Andrzej Klusiewicz!"
+#PEP8
+#import this
+# first_name=input('podaj imię:\n')
+# last_name=input('podaj nazwisko:\n')
+# print("Witaj "+first_name+" "+last_name+"!")
+# print("Witaj {} {}!".format(first_name,last_name))
+# print(f'Witaj {first_name} {last_name}!')
+#
+
+#2. BMI= masa/(wzrost*wzrost) . Napisz program który odbierze od użytkownika jego masę
+# w kilogramach i wzrost w metrach, wyliczy i wypisze BMI.
+
+# print(80/pow(1.76,2))
+# print(round(80/pow(1.76,2),2))
+# print(80/(1.76*1.76))
