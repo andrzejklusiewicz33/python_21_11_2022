8aa81c44ec9206a661ddb4cd8b033cfe7c221a6f
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2022-11-21 09:53:58 +0100

    zadanie 1

--- kod/main.py
+++ kod/main.py
@@ -2,3 +2,38 @@
 #dekoratory
 #obiektowość
 #Faker
+# print("hello")
+# print('hello')
+# print("Mc'Donalds")
+# x='koza'
+# print(x,type(x))
+# x=10
+# print(x,type(x))
+# x=1.3
+# print(x,type(x))
+#x='nietoperz'
+#print('x='+x)
+#x=10
+#print('x='+x)
+#print('x='+str(x))
+# x=10
+# y=66
+# print("x={} y={}".format(x,99))
+# print(f'x={x} y={y}')
+#jakiś komentarz
+#zwierze=input('podaj nazwę zwierza:\n')
+#print(f'wybrane zwierze to {zwierze}')
+
+# x=input('podaj x:\n')
+# #print(x/3)
+# print(type(x))
+
+#print('hajs '*100)
+
+#x=int(input('podaj x:\n'))
+# x=float(  input('podaj x:\n')    )
+# print(type(x))
+# print(x/3)
+
+#1. Napisz program który przyjmie od uzyszkodnika jego imie oraz nazwisko, a nastepnie
+#wyswietli komunikat typu "Witaj Andrzej Klusiewicz!"
