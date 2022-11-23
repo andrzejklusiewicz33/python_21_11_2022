454874e022f9ac54f54452abed8dba5008c33346
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2022-11-23 14:14:02 +0100

    zadanie 54

--- web/app.py
+++ web/app.py
@@ -1,18 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():  # put application's code here
-    return 'Hello World!'
+    #return '<h1>Strona główna</h1>'
+    return render_template("index.html")
 
-@app.route('/help')
-def help():
-    return "<h1>Strona pomocy</h1>"
+@app.route('/show_products')
+def show_products():
+    return "<h1>Lista produktów</h1>"
+
+@app.route('/about')
+def about():
+    return "<h1>Strona o programie</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True,port=80)
 
 #53. Zadbaj o to by Twoja aplikacja miala podstrony : / (strona glowna), /show_products, /about
-# Na każdej stronie powinien pojawic sie napis okreslajacy na jakiej stronie jestesmy
+# Na każdej stronie powinien pojawic sie napis okreslajacy na jakiej stronie jestesmy.
+# Włacz tryb debug i zmien port na 80
+
+#54. Zadbaj o to by wszystkie ekrany zamiast zwracac html w returnie zwracaly pliki html
+#kazdy ekran powinien miec swoj plik html
