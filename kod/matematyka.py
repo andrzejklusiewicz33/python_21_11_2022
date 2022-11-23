204d0c4fb19efbee7751a2066fa39252675d6d71
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2022-11-23 13:44:34 +0100

    koniec

---
+++ kod/matematyka.py
@@ -0,0 +1,14 @@
+def dodawanie(a,b):
+    return a+b
+
+def odejmowanie(a,b):
+    return a-b
+
+def mnozenie(a,b):
+    return a*b
+
+def dzielenie(a,b):
+    return a/b
+
+
+print('kod nie objęty żadną funkcją....')
