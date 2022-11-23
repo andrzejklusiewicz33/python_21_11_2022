891d714539e4d38a276c001d973685c9f243fc2d
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2022-11-23 14:04:01 +0100

    zadanie 53

--- web/app.py
+++ web/app.py
@@ -4,9 +4,15 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def index():  # put application's code here
     return 'Hello World!'
 
+@app.route('/help')
+def help():
+    return "<h1>Strona pomocy</h1>"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True,port=80)
+
+#53. Zadbaj o to by Twoja aplikacja miala podstrony : / (strona glowna), /show_products, /about
+# Na każdej stronie powinien pojawic sie napis okreslajacy na jakiej stronie jestesmy
