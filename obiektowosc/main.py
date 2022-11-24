23e5d33b275f6a574b4cbd22fe9dad08c0cc9ab4
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2022-11-24 13:40:37 +0100

    zadanie 69

--- obiektowosc/main.py
+++ obiektowosc/main.py
@@ -42,3 +42,48 @@
 # Klasa ta powinna zawierać też metodę "wyswietl" wypisującą dane z obiektu na konsoli
 # Stwórz dwa obiekty tej klasy (uzupelnij tez dane)
 # i korzystajac  z metody "wyświetl" wyswietl na konsoli ich zawartość.
+# 
+# class Samochod:
+#     marka=None
+#     model=None
+#     rejestracja=None
+#     def wyswietl(self):
+#         print(f'marka={self.marka}, model={self.model}, rejestracja={self.rejestracja}')
+# 
+# s1=Samochod()
+# s1.marka='Kałdi'
+# s1.model="A4"
+# s1.rejestracja="WWL 12345"
+# 
+# s2=Samochod()
+# s2.marka='Czarny'
+# s2.model='Ciągnik'
+# s2.rejestracja='POJ 2400'
+# 
+# s1.wyswietl()
+# s2.wyswietl()
+#
+# class Person:
+#     first_name=None
+#     last_name=None
+#
+#     def set_values(self,fn,ln):
+#         self.first_name=fn
+#         self.last_name=ln
+#
+#     def show_me(self):
+#         print(f'first_name={self.first_name}, last_name={self.last_name}')
+#
+# p1=Person()
+# p1.set_values('Twoja','Stara')
+# p1.show_me()
+
+#69. Zadbaj o to by klasa Samochod posiadała metodę pozwalającą ustawić wartości wszystkich pól.
+# Jej przykładowe wywołanie: s1.ustaw_wartosci(‘Renault’,’Kadjar’,’WE968RP’)
+
+class Samochod:
+    marka=None
+    model=None
+    rejestracja=None
+    def wyswietl(self):
+        print(f'marka={self.marka}, model={self.model}, rejestracja={self.rejestracja}')
